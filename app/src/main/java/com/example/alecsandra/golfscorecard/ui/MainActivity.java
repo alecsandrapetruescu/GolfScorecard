8c5d91afcc5bf343180be93fb33a2857607d31ca
Alecsandra <alecsandra.petruescu@gmail.com>
2016-09-22 21:07:28 +0300

    use SharedPreferences and gson

--- app/src/main/java/com/example/alecsandra/golfscorecard/ui/MainActivity.java
+++ app/src/main/java/com/example/alecsandra/golfscorecard/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.alecsandra.golfscorecard.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 import com.example.alecsandra.golfscorecard.R;
 import com.example.alecsandra.golfscorecard.adapters.ScoreAdapter;
 import com.example.alecsandra.golfscorecard.model.Score;
+import com.google.gson.Gson;
 
 import java.util.Arrays;
 
@@ -25,6 +28,10 @@ public class MainActivity extends Activity {
     @BindView(android.R.id.list) ListView mListView;
     @BindView(android.R.id.empty) TextView mEmptyTextView;
 
+    private static final String PREFS_FILE = "com.teamtreehouse.golfscorecard.preferences";
+    private static final String KEY_SCORES = "KEY_SCORES";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
     private Score[] mScores;
 
     @Override
@@ -34,25 +41,28 @@ public class MainActivity extends Activity {
 
         ButterKnife.bind(this);
 
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
 
-                Score[] scores = new Score[18];
+        Gson gson = new Gson();
+        String json = mSharedPreferences.getString(KEY_SCORES, "");
+        if(json == ""){
+            Score[] scores = new Score[18];
 
-                for(int i = 0; i < 18; i++ ){
+            for(int i = 0; i < 18; i++ ){
 
-                    Score score = new Score();
-                    score.setScoreName("Hole " + i + ": ");
-                    score.setScoreValue(0);
+                Score score = new Score();
+                score.setScoreName("Hole " + i + ": ");
+                score.setScoreValue(0);
 
-                    scores[i] = score;
-                }
-                mScores = Arrays.copyOf(scores, scores.length, Score[].class);
+                scores[i] = score;
             }
-        });
+            mScores = Arrays.copyOf(scores, scores.length, Score[].class);
+        } else {
+            Log.d("onCreate build ", "json : " + json);
+            mScores = gson.fromJson(json, Score[].class);
+        }
 
-        Log.d("onCreate build ", "length of scores : " + mScores.length);
         ScoreAdapter adapter = new ScoreAdapter(this, mScores);
         mListView.setAdapter(adapter);
         mListView.setEmptyView(mEmptyTextView);
@@ -66,5 +76,17 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
 
+        if (mScores != null){
+            Gson gson = new Gson();
+            String json = gson.toJson(mScores);
+            Log.d("onPause build ", "json : " + json);
+            mEditor.putString(KEY_SCORES, json);
+            mEditor.apply();
+        }
+
+    }
 }
