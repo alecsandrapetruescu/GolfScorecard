3b52740d91e416c0141c090d6f5792f1785ae804
Alecsandra <alecsandra.petruescu@gmail.com>
2016-09-24 21:47:41 +0300

    added action bar, clear all scores

--- app/src/main/java/com/example/alecsandra/golfscorecard/ui/MainActivity.java
+++ app/src/main/java/com/example/alecsandra/golfscorecard/ui/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.alecsandra.golfscorecard.ui;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 import com.example.alecsandra.golfscorecard.R;
 import com.example.alecsandra.golfscorecard.adapters.ScoreAdapter;
@@ -23,7 +26,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @BindView(android.R.id.list) ListView mListView;
     @BindView(android.R.id.empty) TextView mEmptyTextView;
@@ -39,6 +42,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         ButterKnife.bind(this);
 
         mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
@@ -46,18 +52,10 @@ public class MainActivity extends Activity {
 
         Gson gson = new Gson();
         String json = mSharedPreferences.getString(KEY_SCORES, "");
-        if(json == ""){
-            Score[] scores = new Score[18];
-
-            for(int i = 0; i < scores.length; i++ ){
-                int scoreNameValue = i+1;
-
-                Score score = new Score();
-                score.setScoreName("Hole " + scoreNameValue + ": ");
-                score.setScoreValue(0);
-                scores[i] = score;
-            }
+        if (json.isEmpty()) {
+            Score[] scores = buildScoreCard();
             mScores = Arrays.copyOf(scores, scores.length, Score[].class);
+            Log.d("onCreate build ", "empty json : ");
         } else {
             Log.d("onCreate build ", "json : " + json);
             mScores = gson.fromJson(json, Score[].class);
@@ -76,11 +74,25 @@ public class MainActivity extends Activity {
 
     }
 
+    @NonNull
+    private Score[] buildScoreCard() {
+        Score[] scores = new Score[18];
+        for (int i = 0; i < scores.length; i++) {
+            int scoreNameValue = i + 1;
+
+            Score score = new Score();
+            score.setScoreName("Hole " + scoreNameValue + ": ");
+            score.setScoreValue(0);
+            scores[i] = score;
+        }
+        return scores;
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
 
-        if (mScores != null){
+        if (mScores != null) {
             Gson gson = new Gson();
             String json = gson.toJson(mScores);
             Log.d("onPause build ", "json : " + json);
@@ -88,5 +100,35 @@ public class MainActivity extends Activity {
             mEditor.apply();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                mScores = buildScoreCard();
+                ScoreAdapter adapter = new ScoreAdapter(MainActivity.this, mScores);
+                mListView.setAdapter(adapter);
+
+                Gson gson = new Gson();
+                String json = gson.toJson(mScores);
+
+                mEditor.putString(KEY_SCORES, json);
+                mEditor.apply();
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
